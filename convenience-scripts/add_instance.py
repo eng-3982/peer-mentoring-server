d06a721bc3c737cef9afcb300c22f635e2148ea8
Claire Durand <claire.durand@temple.edu>
2016-01-26 23:24:30 -0500

    Added update feature

--- convenience-scripts/add_instance.py
+++ convenience-scripts/add_instance.py
@@ -15,14 +15,30 @@ coll = db.dataset
 # Otherwise, if the user entered an odd number of arguments, the key value pairs match up.
 # Print out an error message to the user, inviting him or her to try again.
 if(len(sys.argv)%2 == 1):
-    for i in range(1,len(sys.argv),2):
-        key = sys.argv[i]
-        value = sys.argv[i+1]
-        coll.insert_one({key:value})
+    # Save the first key:value pair for reference
+    key1 = sys.argv[1]
+    value1 = sys.argv[2]
+    coll.insert_one({key1:value1})
+
+    # Update the instance with any remaining key value pairs
+    # (Only occurs if user is trying to insert more than one key:value pair per document)
+    if(len(sys.argv)>3):
+        for i in range(3,len(sys.argv),2):
+            key = sys.argv[i]
+            value = sys.argv[i+1]
+            
+            coll.update(
+                { key1 : value1 },
+                { "$set": { key : value } }
+            )
 else:
     print "ERROR >>> You've entered an odd number of arguments. Please enter key:value pairs"
-
-
+"""
+coll.update(
+    {"name": "Claire" },
+    {"$set": {"major": "Electrical Engineering"} }
+)
+"""
    
 
 
