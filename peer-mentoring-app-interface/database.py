26159b5c12fede49af1f0cfc11729cc72a3243e6
Daniel <daniel.douglas@temple.edu>
2016-02-25 18:39:48 -0500

    succesful user add/authentication

--- peer-mentoring-app-interface/database.py
+++ peer-mentoring-app-interface/database.py
@@ -3,27 +3,34 @@ from pymongo import MongoClient
 
 client = MongoClient()
 db = client.db_00
-coll = db.dataset
+coll = db.users
+
 
 # define the function to print out all data 
-# in database primer, collection dataset
-def data():
-    print "Innerds of data"
-    cursor = coll.find({},{ '_id':0 })
+# in database db, coll
+#
+def data(collection_name):
+    # Create the collection object
+    #
+    collexion = db[collection_name]
+
+    # Import database minus the '_id' field
+    #
+    cursor = collexion.find({},{ '_id':0 })
     result = {}
-    i = 0
+    
+    # Iterate over collection to create dictionary
+    #
     for c in cursor:
-        result[str(i)]=c
-        i+=1
-    print result
-    return jsonify(**result)
+        result[c['name']]=c
+    return result
 
 # Function to add values to the database
 #
 def post(name,passwd):
-    
+
     print "POSTING TO DATABASE"
-    x = coll.insert({"name":name, 'pass':passwd})
+    x = coll.insert({'name':name, 'password':passwd})
     return 'Entry was added successfully\n'
 
 if __name__ == '__main__':
--- server/app_v01.py
+++ server/app_v01.py
@@ -5,7 +5,7 @@
 
 import sys
 import os
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 from flask_login import LoginManager, UserMixin, login_required
 
 # Move to the directory that contains the interfacing script to import it
@@ -22,6 +22,67 @@ app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
+# Define User class. User inherits from UserMixin class
+# which we will "overload" (C++ term)
+#
+class User(UserMixin):
+    # Import user data
+    #
+    user_database = database.data('users')
+
+    # Initialize class parameters
+    #
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    # Define a classmethod for easy repeatability
+    #
+    @classmethod
+    def get(cls, id):
+        return cls.user_database.get(id)
+
+
+# Provide method for login manager to load a user
+#
+@login_manager.request_loader
+def load_user(request):
+    # Look for token in Authorization headers or request
+    # arguments
+    #
+    print 'XXXX', request.authorization
+    print 'YYYY', request.headers
+    token = request.headers.get('Username'),request.headers.get('Password')
+    if token is None:
+        token = request.args.get('token')
+
+    # Once we have the token, check for a match
+    #
+    if token is not None:
+        print 'token is ', token
+        username, password = token[0],token[1] # ASSUMING A NAIVE TOKEN
+        
+        # get the user information from class parameters
+        #
+        user_entry = User.get(username)
+        print 'user entry is ', user_entry
+        
+        if (user_entry is not None):           
+            # Create User() object from information
+            #
+            user = User(user_entry['name'], user_entry['password'])
+            
+            print 'user is ', user
+            # If authentication info is correct, return user instance
+            #
+            if (user.password == password):
+                return user
+        # If authenticaton fails, return None
+        #
+        return None
+            
+
 # Default URL operations
 #
 @app.route('/')
@@ -31,19 +92,21 @@ def home_page():
     #
     return 'Welcome to Peer-Mentoring App\n'
 
+
+# Data fetch and post operations
+#
 @app.route('/data/', methods=['POST', 'GET'])
 def data():
     
-    # Data directory welcome message
-    #
-    print "Welcome to data"
-    
+    print 'HEADERS', request.headers
+
     # If the request method is GET..
     #
     if request.method == 'GET':
+        
         # Print the key/value data in the database
         #
-        return database.data()
+        return jsonify(database.data('users'))
 
 
     # If the request method is POST
@@ -51,9 +114,27 @@ def data():
     else:
         # Add the key/values in request to the database
         #
-        name = request.form['name']
-        passwd = request.form['pass']
-        return database.post(name,passwd)
+        name = request.headers.get('Username')
+        password = request.headers.get('Password')
+        print name
+        print type(name)
+        # If both fields are present in the request,
+        # add the user information to the database
+        #
+        if (name and password):
+            return database.post(name,password)
+        else:
+            # Raise error code 400=BAD REQUEST
+            #
+            return Response(response="Required Field's Empty", status=200)
+
+
+# Authenticated user operation
+#
+@app.route('/private/', methods=['GET', 'POST'])
+@login_required    # this line makes the route requre authentication
+def private():
+    return Response(response='success', status=200)
 
 if __name__ == '__main__':
     # Run the app on all interfaces in debug mode
